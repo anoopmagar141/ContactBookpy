6df133f9a5766d79fbeffb30bbfa559be6530a23
[Anup  Magar] <am6956275@gmail.com>
2025-03-26 18:59:03 +0545

    Add Contact class with initialization and string representation

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+    def __str__(self):
+        return f"Name: {self.name} | Phone: {self.phone} | Email: {self.email}"
+
